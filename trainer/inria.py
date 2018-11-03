9b9916bf549b1a5a5bcf24dfce2f210b703cac52
Mateusz Puczynski <mateusz.puczynski@sap.com>
2018-11-03 18:01:16 +0100

    prevAdam

--- trainer/inria.py
+++ trainer/inria.py
@@ -74,11 +74,13 @@ def train_model(train_file='inzynierka',
     model.add(Activation('sigmoid'))
 
     model.compile(loss='binary_crossentropy',
-                  optimizer='rmsprop',
+                  optimizer='adam',
                   metrics=['accuracy'])
     model.summary()
 
-    train_datagen = ImageDataGenerator(rescale=1. / 255)
+    train_datagen = ImageDataGenerator(rescale=1. / 255, shear_range=0.2,
+                                       zoom_range=0.2,
+                                       horizontal_flip=True)
     test_datagen = ImageDataGenerator(rescale=1. / 255)
 
     train_generator = train_datagen.flow_from_directory(
