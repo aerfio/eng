019c0f4977af798945d40a5ffd62194a3319ceb4
Mateusz Puczynski <mateusz.puczynski@sap.com>
2018-11-27 09:08:32 +0100

    a128only

--- trainer/inria.py
+++ trainer/inria.py
@@ -61,11 +61,6 @@ def train_model(train_file='inzynierka',
     model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(64, (5, 5)))
-    model.add(Activation('selu'))
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
     model.add(Conv2D(128, (5, 5)))
     model.add(Activation('selu'))
     model.add(BatchNormalization())
