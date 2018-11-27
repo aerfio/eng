3c1770c57d6ddcf02b42c1a5d7344a924c9f4534
Mateusz Puczynski <mateusz.puczynski@sap.com>
2018-11-27 09:14:09 +0100

    changedFilters

--- trainer/inria.py
+++ trainer/inria.py
@@ -66,11 +66,6 @@ def train_model(train_file='inzynierka',
     model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(64, (3, 3)))
-    model.add(Activation('selu'))
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
     model.add(Flatten())
     model.add(Dense(64))
     model.add(Activation('selu'))
